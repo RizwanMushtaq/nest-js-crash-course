71a16015357bd738ea8299fdb06e35074c827883
Rizwan Mushtaq <rizwan.mushtaq@ottonova.de>
2022-06-09 19:36:40 +0200

    adding users controller

---
+++ src/users/users.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get()
+  getUsers(): any {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string): any {
+    return this.usersService.findById(Number(id));
+  }
+
+  // @Post()
+  // createUser(): any {
+  //   return this.usersService.createUser()
+  // }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UsersService {
+  private users: any = [
+    {
+      id: 0,
+      name: 'Rizwan',
+    },
+  ];
+
+  findAll() {
+    return this.users;
+  }
+
+  findById(userId: number) {
+    return this.users.find((user) => user.id === userId);
+  }
+
+  createUser(name: string) {
+    const newUser = {
+      id: Date.now(),
+      name,
+    };
+    this.users.push(newUser);
+  }
+}

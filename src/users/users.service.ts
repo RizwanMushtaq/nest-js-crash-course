d7c5a0f0ab12a8f014f7362869e6afca3294ca8e
Rizwan Mushtaq <rizwan.mushtaq@ottonova.de>
2022-06-10 19:49:37 +0200

    adding params, body, queryparams, swagger

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
@@ -7,21 +9,33 @@ export class UsersService {
       id: 0,
       name: 'Rizwan',
     },
+    {
+      id: 1,
+      name: 'Rizwan',
+    },
+    {
+      id: 2,
+      name: 'test01',
+    },
   ];
 
-  findAll() {
+  findAll(name?: string): User[] {
+    if (name) {
+      return this.users.filter((user) => user.name === name);
+    }
     return this.users;
   }
 
-  findById(userId: number) {
+  findById(userId: number): User {
     return this.users.find((user) => user.id === userId);
   }
 
-  createUser(name: string) {
+  createUser(createUserDto: CreateUserDto): User {
     const newUser = {
       id: Date.now(),
-      name,
+      ...createUserDto,
     };
     this.users.push(newUser);
+    return newUser;
   }
 }
